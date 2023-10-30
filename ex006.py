872e56010e34046541bcbf00a79f7c25e0ee9ce4
João Pedro <joaopedrosilveira59@gmail.com>
2023-10-30 15:30:27 -0300

    update

--- ex006.py
+++ ex006.py
@@ -6,7 +6,5 @@ r = n ** (1/2)
 print('O dorbo, o triplo e a raiz de {} é {}, {} e {}'.format(n, d, t, r))
 
 # Segunda forma
-'''
 n = int(input('Digite umm número: '))
-print('O dorbo, o triplo e a raiz de {} é {}, {} e {}'.format(n, (n*2), (n*3), (n**(1/2)))
-'''
+print('O dorbo, o triplo e a raiz de {} é {}, {} e {}'.format(n, (n*2), (n*3), pow(n, (1/2))))
